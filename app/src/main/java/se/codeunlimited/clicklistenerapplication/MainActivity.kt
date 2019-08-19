67114523f0cdba4dbb4db83d56f961d9dae77bce
Peter Törnhult <peter.tornhult@bookbeat.com>
2019-08-19 14:22:19 +0200

    Add LiveData example

--- app/src/main/java/se/codeunlimited/clicklistenerapplication/MainActivity.kt
+++ app/src/main/java/se/codeunlimited/clicklistenerapplication/MainActivity.kt
@@ -3,9 +3,13 @@ package se.codeunlimited.clicklistenerapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val liveDataCounter = MutableLiveData<Int>().apply { value = 0 } // Set initial value to update UI
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,5 +19,13 @@ class MainActivity : AppCompatActivity() {
         ex1UpdateUiDirectly.setOnClickListener {
             (it as Button).text = "Clicked: ${++clickCounter} times"
         }
+
+        ex2UpdateUiDirectly.setOnClickListener {
+            liveDataCounter.value = (liveDataCounter.value ?: 0) + 1
+        }
+
+        liveDataCounter.observe(this, Observer {
+            ex2UpdateUiDirectly.text = "Clicked: $it times"
+        })
     }
 }
