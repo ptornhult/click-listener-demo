7a0269b246adae90683ddef400a389fa748dbf24
Peter Törnhult <peter.tornhult@bookbeat.com>
2019-08-19 13:30:21 +0200

    Add ex 1. Button which updates the UI directly on Click

--- app/src/main/java/se/codeunlimited/clicklistenerapplication/MainActivity.kt
+++ app/src/main/java/se/codeunlimited/clicklistenerapplication/MainActivity.kt
@@ -2,11 +2,18 @@ package se.codeunlimited.clicklistenerapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var clickCounter = 0
+        val ex1UpdateUiDirectly = findViewById<Button>(R.id.ex1UpdateUiDirectly)
+        ex1UpdateUiDirectly.setOnClickListener {
+            (it as Button).text = "Clicked: ${++clickCounter} times"
+        }
     }
 }
